c36743e5a50fe4285de75489e8f82611e61aaa83
Marcelo Santos <rocha.mer21@gmail.com>
2022-07-13 11:43:03 -0300

    Stage 2/5: Print your name

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,7 +1,16 @@
 package bot
 
+import java.util.Scanner
+
+
 fun main() {
-    // write your code here
+
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    // reading a name
+    val yourName = readln()
+
+    println("What a great name you have, $yourName!")
 }
